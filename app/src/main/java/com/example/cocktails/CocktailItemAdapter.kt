56a3e3524e39dc10ca1adc2d7f8617bab063fad2
zoharhen <49272962+zoharhen@users.noreply.github.com>
2020-07-12 18:36:08 +0300

    favorites icon + save data to sp

--- app/src/main/java/com/example/cocktails/CocktailItemAdapter.kt
+++ app/src/main/java/com/example/cocktails/CocktailItemAdapter.kt
@@ -9,6 +9,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.github.ivbaranov.mfb.MaterialFavoriteButton
+
 
 class CocktailItemAdapter internal constructor(context: Context, data: List<Cocktail>) :
     RecyclerView.Adapter<CocktailItemAdapter.ViewHolder>() {
@@ -36,24 +38,33 @@ class CocktailItemAdapter internal constructor(context: Context, data: List<Cock
         var header: TextView = itemView.findViewById(R.id.item_title)
         var description: TextView = itemView.findViewById(R.id.item_description)
         var imageView: ImageView = itemView.findViewById(R.id.item_clipart)
+        var favorite: MaterialFavoriteButton = itemView.findViewById(R.id.favorite_button)
 
         init {
             itemView.setOnClickListener{
                 onItemClick?.invoke(items[adapterPosition])
             }
+
+            favorite.setOnFavoriteChangeListener{ btn: MaterialFavoriteButton, value: Boolean ->
+
+                (cnt.applicationContext as Cocktails).mFavorites.edit()
+                    .putBoolean(adapterPosition.toString(), value).apply()
+            }
         }
 
         @SuppressLint("SetTextI18n")
         fun bind(cocktail: Cocktail) {
+            val applicationContext = (cnt.applicationContext as Cocktails)
             header.text = cocktail.name
             description.text = "Type: ${cocktail.type}"
-            (cnt.applicationContext as Cocktails).mStorageRef
-                .child("cliparts/" + cocktail.image + ".png")
+            applicationContext.mStorageRef.child("cliparts/" + cocktail.image + ".png")
                 .downloadUrl.addOnSuccessListener {
                     Glide.with(cnt)
                         .load(it)
                         .into(imageView)
                 }
+            favorite.isFavorite = applicationContext.mFavorites.getBoolean(adapterPosition.toString(), false)
+
             // get image from local storage (drawable folder):
             //            val imageId = cnt.resources.getIdentifier(cocktail.image, "drawable", cnt.packageName);
             //            imageView.setImageResource(imageId)
--- app/src/main/java/com/example/cocktails/Cocktails.kt
+++ app/src/main/java/com/example/cocktails/Cocktails.kt
@@ -1,18 +1,23 @@
 package com.example.cocktails
 
 import android.app.Application
-import com.google.firebase.auth.FirebaseAuth
+import android.content.Context
+import android.content.SharedPreferences
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
 
 class Cocktails : Application() {
 
+    val FAVORITES = "FAVORITES"
+
     // shared data
+    lateinit var mFavorites: SharedPreferences
     lateinit var mStorageRef: StorageReference
 
     override fun onCreate() {
         super.onCreate()
+        mFavorites = this.getSharedPreferences(FAVORITES, Context.MODE_PRIVATE)
         mStorageRef = FirebaseStorage.getInstance().reference
     }
 }
