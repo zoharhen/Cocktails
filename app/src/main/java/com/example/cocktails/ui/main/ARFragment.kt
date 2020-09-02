a53db20bfe12cc5cb3e2abc22cfa95146e3875d8
shahaf.hermann <shahaf.hermann@cs.huji.ac.il>
2020-09-02 17:27:56 +0300

    Added a cocktail class 3D model and rendering to the fragment. Needs alot of testing

--- app/src/main/java/com/example/cocktails/ui/main/ARFragment.kt
+++ app/src/main/java/com/example/cocktails/ui/main/ARFragment.kt
@@ -4,21 +4,21 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Context.ACTIVITY_SERVICE
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.cocktails.Cocktail
-import com.example.cocktails.Cocktails
 import com.example.cocktails.R
-import com.example.cocktails.ScrollingActivity
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import java.util.function.Consumer
 
 
 class ARFragment(context: Context) : Fragment() {
@@ -26,32 +26,49 @@ class ARFragment(context: Context) : Fragment() {
     private var TAG: String = "COMPATIBILITY"
     private var MIN_OPENGL_VERSION: Double = 3.0
 
-//    private lateinit var pageViewModel: PageViewModel
+    var arFragment: ArFragment? = null
+    var glassRenderable: ModelRenderable? = null
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    @Override
+//    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).
-//        apply {setIndex(arguments?.getInt(PlaceholderFragment.ARG_SECTION_NUMBER) ?: 1)
-//        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    @Override
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        val activity: Activity = requireActivity()
+
         // Check compatibility to AR. if none, just return.
-        if (!checkCompatibility(requireActivity())) {
+        if (!checkCompatibility(activity)) {
             return super.onCreateView(inflater, container, savedInstanceState)
         }
 
         // Create the AR layout and return it
         val root = inflater.inflate(R.layout.ar_layout, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-//        pageViewModel.text.observe(viewLifecycleOwner, Observer<String> {
-//            textView.text = it
-//        })
+
+        arFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.ux_fragment)
+                as ArFragment
+        ModelRenderable.builder()
+            .setSource(activity, Uri.parse("cocktail_glass.sfb"))
+            .build()
+            .thenAccept(Consumer { renderable: ModelRenderable ->
+                glassRenderable = renderable
+            })
+            .exceptionally {
+                val toast: Toast =
+                    Toast.makeText(activity, "Unable to load renderable", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.show()
+                null
+            }
 
         return root
     }
@@ -63,7 +80,9 @@ class ARFragment(context: Context) : Fragment() {
     fun checkCompatibility(activity: Activity): Boolean {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             Log.e(TAG, "Sceneform requires Android N or later")
-            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG)
+            Toast.makeText(activity,
+                "AR requires Android version" + Build.VERSION_CODES.N + "or later",
+                Toast.LENGTH_LONG)
                 .show()
             activity.finish()
             return false
@@ -74,7 +93,7 @@ class ARFragment(context: Context) : Fragment() {
                 .glEsVersion
         if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
             Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later")
-            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+            Toast.makeText(activity, "AR requires Android 4.3 or higher", Toast.LENGTH_LONG)
                 .show()
             activity.finish()
             return false
