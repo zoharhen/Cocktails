c9440c8ff4785e89be9669b513bfdc65b57ff47f
shahaf.hermann <shahaf.hermann@cs.huji.ac.il>
2020-09-02 19:02:45 +0300

    AR - now with documentation and a GIANT glass

--- app/src/main/java/com/example/cocktails/ui/main/ARFragment.kt
+++ app/src/main/java/com/example/cocktails/ui/main/ARFragment.kt
@@ -8,16 +8,17 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.cocktails.R
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import java.util.function.Consumer
 
 
@@ -54,14 +55,20 @@ class ARFragment(context: Context) : Fragment() {
         // Create the AR layout and return it
         val root = inflater.inflate(R.layout.ar_layout, container, false)
 
+        // Find the AR fragment
         arFragment = childFragmentManager.findFragmentById(R.id.ux_fragment)
                 as ArFragment
+
+        // Build the 3D model. Load it from the sceneform binary file (sfb)
+        // This action is asynchronous.
         ModelRenderable.builder()
             .setSource(activity, Uri.parse("cocktail_glass.sfb"))
             .build()
+                // Once it's built set our renderable
             .thenAccept(Consumer { renderable: ModelRenderable ->
                 glassRenderable = renderable
             })
+                // Error handling
             .exceptionally {
                 val toast: Toast =
                     Toast.makeText(activity, "Sorry, Something went wrong!", Toast.LENGTH_LONG)
@@ -69,6 +76,25 @@ class ARFragment(context: Context) : Fragment() {
                 toast.show()  // Unable to  load renderer
                 null
             }
+
+        // Add the rendered model to the scene
+        // the scene is where the 3D objects are rendered.
+        // HitResult is a ray-cast on the object.
+        // Anchor is a fixed point in the 3D space.
+        // AnchorNode positions itself in the world. This is the first node to be set.
+        arFragment!!.setOnTapArPlaneListener { hitResult: HitResult, _: Plane?, _: MotionEvent? ->
+            if (glassRenderable == null) {
+                return@setOnTapArPlaneListener
+            }
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment!!.arSceneView.scene)
+            val lamp =
+                TransformableNode(arFragment!!.transformationSystem)
+            lamp.setParent(anchorNode)
+            lamp.renderable = glassRenderable
+            lamp.select()
+        }
 
         return root
     }
@@ -81,7 +107,7 @@ class ARFragment(context: Context) : Fragment() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             Log.e(TAG, "Sceneform requires Android N or later")
             Toast.makeText(activity,
-                "This feature requires Android version" + Build.VERSION_CODES.N + "or later",
+                "This feature requires Android " + Build.VERSION_CODES.N + "or later",
                 Toast.LENGTH_LONG)
                 .show()
             activity.finish()
