583f2bbd2fcbd06633279eed0dd9b97115992274
shahaf.hermann <shahaf.hermann@cs.huji.ac.il>
2020-09-03 19:29:18 +0300

    Flipped the tabs

--- app/src/main/java/com/example/cocktails/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/cocktails/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,8 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.example.cocktails.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_1
 )
 
 /**
@@ -21,7 +21,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        if (position == 0) {
+        if (position == 1) {
             return ARFragment(context)
         }
 
