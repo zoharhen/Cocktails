215424fcd3d489ea35f1dc36a8111e1e80563707
enavhasson <enav.hasson@cs.huji.ac.il>
2020-09-06 20:25:54 +0300

    add cocktail by user
    add new activity for adding new cocktail by user while press plus button on the main screen

---
+++ app/src/main/java/com/example/cocktails/ui/main/UserItem.kt
@@ -0,0 +1,13 @@
+package com.example.cocktails.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cocktails.R
+
+class UserItem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_item)
+    }
+}
