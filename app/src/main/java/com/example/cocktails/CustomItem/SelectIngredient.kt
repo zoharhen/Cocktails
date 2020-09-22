54c3b3699ee4237512d4af336ff6c5013f3a4396
enavhasson <enav.hasson@cs.huji.ac.il>
2020-09-22 22:32:02 +0300

    change unit style
    change unit style while adding ingredients to the table

--- app/src/main/java/com/example/cocktails/CustomItem/SelectIngredient.kt
+++ app/src/main/java/com/example/cocktails/CustomItem/SelectIngredient.kt
@@ -1,34 +1,37 @@
 package com.example.cocktails.CustomItem
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.RadioButton
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cocktails.CustomItem.insertIngredients.SelectUnit
+import com.example.cocktails.CustomItem.insertIngredients.UNIT_VAL_KEY
 import com.example.cocktails.R
 import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_select_ingredient.*
 import java.util.*
 import kotlin.collections.ArrayList
 
-val QUANTITY_KEY = "quantity"
+val UNIT_KEY = "quantity"
 val INGREDIENT_KEY = "ingredient"
-val UNIT_KEY = "unit"
+val QUANTITY_KEY = "unit"
+val CURRENT_UNIT_KEY="current_unit_key"
 
 class SelectIngredient : AppCompatActivity() {
-    private lateinit var mQuantity: AutoCompleteTextView
-    private lateinit var mUnit: TextInputLayout
+    private lateinit var mUnit: AutoCompleteTextView
+    private lateinit var mQuantity: TextInputLayout
     private lateinit var mIngredient: AutoCompleteTextView
     private lateinit var mIngredientNew: TextInputLayout
-    private lateinit var mQuantityList: ArrayList<String>
+    private lateinit var mUnitList: ArrayList<String>
     private lateinit var mIngredientList: ArrayList<String>
     val ERROR_MSG_INGREDIENT="This ingredient exist in the collection."
     val ERROR_MSG_EMPTY_INGREDIENT= "error - choose ingredient."
-    val ERROR_MSG_EMPTY_QUANTITY="error - choose quantity."
-    val ERROR_MSG_UNIT = "error - only a integer or float is allow."
+    val ERROR_MSG_EMPTY_UNIT="error - choose quantity."
+    val ERROR_MSG_QUANTITY = "error - only a integer or float is allow."
+    private val REQUEST_CODE_UNIT=2
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,12 +44,19 @@ class SelectIngredient : AppCompatActivity() {
 
     @SuppressLint("CutPasteId")
     private fun initView() {
-        mQuantityList = ArrayList(listOf(*resources.getStringArray(R.array.quantityTypes_array)))
-        val quantityAdapter = ArrayAdapter(this, R.layout.dropdown_menu_popup_item, mQuantityList)
-        mQuantity = findViewById(R.id.quantity_dropdown)
-        mQuantity.setAdapter(quantityAdapter)
+        mUnitList = ArrayList(listOf(*resources.getStringArray(R.array.unitTypes_array)))
+        val quantityAdapter = ArrayAdapter(this, R.layout.dropdown_menu_popup_item, mUnitList)
+        mUnit = findViewById(R.id.unit_dropdown)
+        mUnit.setAdapter(quantityAdapter)
 
-        mUnit = findViewById(R.id.textFieldUnit)
+        findViewById<LinearLayout>(R.id.unit_layout).setOnClickListener{
+            val unitIntent = Intent(this, SelectUnit::class.java)
+            val unitTV : TextView = findViewById(R.id.unit_textView)
+            unitIntent.putExtra(CURRENT_UNIT_KEY,unitTV.text)
+            startActivityForResult(unitIntent, REQUEST_CODE_UNIT)
+        }
+
+        mQuantity = findViewById(R.id.textFieldQuantity)
 
         initIngredientView()
 
@@ -127,36 +137,36 @@ class SelectIngredient : AppCompatActivity() {
 
     private fun returnResult() {
         val intentBack = Intent()
-        intentBack.putExtra(QUANTITY_KEY, mQuantity.text.toString())
-        intentBack.putExtra(UNIT_KEY, getUnit())
+        intentBack.putExtra(UNIT_KEY, mUnit.text.toString())
+        intentBack.putExtra(QUANTITY_KEY, getQuantity())
         intentBack.putExtra(INGREDIENT_KEY, getIngredientVal())
         setResult(RESULT_OK, intentBack)
         finish()
     }
 
     private fun checkValidation(): Boolean {
-        val quantityIsValid = validQuantity()
         val unitIsValid = validUnit()
+        val quantityIsValid = validQuantity()
         val ingredientIsValid = ingredientValidation()
-        return (quantityIsValid && unitIsValid && ingredientIsValid)
-    }
-
-    private fun validQuantity(): Boolean {
-        val value = mQuantity.text
-        if (value.isNullOrEmpty()) {
-            mQuantity.error = ERROR_MSG_EMPTY_QUANTITY
-            return false
-        }
-        mQuantity.error = null
-        return true
+        return (unitIsValid && quantityIsValid && ingredientIsValid)
     }
 
     private fun validUnit(): Boolean {
-        if (getUnit().isEmpty() || getUnit().length > 10) {
-            textFieldUnit.error = ERROR_MSG_UNIT
+        val value = mUnit.text
+        if (value.isNullOrEmpty()) {
+            mUnit.error = ERROR_MSG_EMPTY_UNIT
             return false
         }
-        textFieldUnit.error = null
+        mUnit.error = null
+        return true
+    }
+
+    private fun validQuantity(): Boolean {
+        if (getQuantity().isEmpty() || getQuantity().length > 10) {
+            textFieldQuantity.error = ERROR_MSG_QUANTITY
+            return false
+        }
+        textFieldQuantity.error = null
         return true
     }
 
@@ -170,10 +180,10 @@ class SelectIngredient : AppCompatActivity() {
         return true
     }
 
-    private fun getUnit(): String {
+    private fun getQuantity(): String {
         // Get input text
         //todo trim
-        return mUnit.editText?.text.toString().trim()
+        return mQuantity.editText?.text.toString().trim()
     }
 
     private fun getIngredientVal():String{
@@ -181,6 +191,13 @@ class SelectIngredient : AppCompatActivity() {
             mIngredient.text.toString()
         } else{
             mIngredientNew.editText?.text.toString()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_UNIT && resultCode == Activity.RESULT_OK && data != null) {
+            val unitTV : TextView = findViewById(R.id.unit_textView)
+            unitTV.text = data.getStringExtra(UNIT_VAL_KEY)
         }
     }
 
--- app/src/main/java/com/example/cocktails/CustomItem/UserItemLevel2.kt
+++ app/src/main/java/com/example/cocktails/CustomItem/UserItemLevel2.kt
@@ -2,30 +2,35 @@ package com.example.cocktails.CustomItem
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
+import android.text.InputType
 import android.view.Gravity
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cocktails.R
 
 
 class UserItemLevel2 : AppCompatActivity() {
-    lateinit var tableRow: TableRow
-    lateinit var tableLayout: TableLayout
-    val REQUEST_CODE_SELECT_INGREDIENTS = 1
-    var counterRowTable: Int = -1
+    private lateinit var ingredientTableRow: TableRow
+    private lateinit var ingredientTableLayout: TableLayout
+    private lateinit var preparationTableRow: TableRow
+    private lateinit var preparationTableLayout: TableLayout
+    private val REQUEST_CODE_SELECT_INGREDIENTS = 1
+    private var counterIngredientRowTable: Int = 0
+    private var counterPreparationRowTable: Int = 0
+
+    //    private  var preparationList:ArrayList<String> = arrayListOf()
+    private val NEW_VALUE = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_item_level2)
-        initTable()
-
+        initIngredientsTable()
+        initPreparationTable()
         //todo
         //initToolBar()
     }
@@ -35,14 +40,14 @@ class UserItemLevel2 : AppCompatActivity() {
 //    }
 
     @SuppressLint("ResourceAsColor")
-    private fun initTable() {
-        tableLayout = findViewById(R.id.ingredient_table)
-        tableRow = findViewById(R.id.table_row)
-        counterRowTable = -1
-        tableLayout.setColumnStretchable(0, true)
-        tableLayout.setColumnStretchable(1, true)
-        tableLayout.setColumnStretchable(2, true)
-        tableLayout.setColumnStretchable(3, true)
+    private fun initIngredientsTable() {
+        ingredientTableLayout = findViewById(R.id.ingredient_table)
+        ingredientTableRow = findViewById(R.id.table_row)
+        counterIngredientRowTable = -1
+        ingredientTableLayout.setColumnStretchable(0, true)
+        ingredientTableLayout.setColumnStretchable(1, true)
+        ingredientTableLayout.setColumnStretchable(2, true)
+        ingredientTableLayout.setColumnStretchable(3, true)
 
         val addRowButton: View = findViewById(R.id.adding_new_row)
         addRowButton.setOnClickListener {
@@ -51,19 +56,32 @@ class UserItemLevel2 : AppCompatActivity() {
 
     }
 
+    private fun initPreparationTable() {
+        preparationTableLayout = findViewById(R.id.preparation_table)
+        preparationTableRow = findViewById(R.id.table_row_preparation)
+        counterPreparationRowTable = -1
+        ingredientTableLayout.setColumnStretchable(0, true)
+        ingredientTableLayout.setColumnStretchable(1, true)
+
+        val addPreparationRowButton: View = findViewById(R.id.adding_new_row_preparation)
+        addPreparationRowButton.setOnClickListener {
+            showAddStepDialog(this)
+        }
+    }
+
     private fun startSelectIngredient() {
         val intent = Intent(this, SelectIngredient::class.java)
         startActivityForResult(intent, 1)
     }
 
-    private fun addNewRow(quantityVal: String, unitVal: String, ingredientVal: String) {
-        tableRow = TableRow(this)
+    private fun addNewIngredientRow(quantityVal: String, unitVal: String, ingredientVal: String) {
+        ingredientTableRow = TableRow(this)
         val scrNumRow = TextView(this)
         val scrQuantity = TextView(this)
         val scrUnit = TextView(this)
         val scrIngredient = TextView(this)
 
-        scrNumRow.text = (counterRowTable + 1).toString()
+        scrNumRow.text = (counterIngredientRowTable + 1).toString()
         scrNumRow.gravity = Gravity.CENTER
         scrNumRow.textSize = 15F
 
@@ -79,25 +97,70 @@ class UserItemLevel2 : AppCompatActivity() {
         scrIngredient.gravity = Gravity.CENTER
         scrIngredient.textSize = 15F
 
-        tableRow.addView(scrNumRow)
-        tableRow.addView(scrQuantity)
-        tableRow.addView(scrUnit)
-        tableRow.addView(scrIngredient)
-        tableRow.setPadding(10, 10, 10, 10)
-        tableRow.setBackgroundColor(resources.getColor(R.color.rowColor))
-        tableLayout.addView(tableRow)
+        ingredientTableRow.addView(scrNumRow)
+        ingredientTableRow.addView(scrQuantity)
+        ingredientTableRow.addView(scrUnit)
+        ingredientTableRow.addView(scrIngredient)
+        ingredientTableRow.setPadding(10, 10, 10, 10)
+        ingredientTableRow.setBackgroundColor(resources.getColor(R.color.rowColor))
+        ingredientTableLayout.addView(ingredientTableRow)
     }
 
+
+    private fun showAddStepDialog(c: Context) {
+        val inputText = EditText(c)
+        inputText.setRawInputType(InputType.TYPE_CLASS_TEXT)
+        val dialog = AlertDialog.Builder(c)
+            .setView(inputText)
+            .setPositiveButton("Add") { _, _ ->
+                val input = inputText.text.toString()
+                addNewStepRow(input)
+            }
+            .setNegativeButton("Cancel", null)
+            .setTitle("Add a new step")
+        //todo for edit
+//        if (index != NEW_VALUE) { //phone num clear or never insert
+//            dialog.setMessage("current step: "+ preparationList[index])
+//        }
+        dialog.create()
+        dialog.show()
+    }
+
+    private fun addNewStepRow(stepInput: String) {
+        preparationTableRow = TableRow(this)
+        val scrNumRow = TextView(this)
+        val scrStep = TextView(this)
+
+        scrNumRow.text = (counterPreparationRowTable + 1).toString()
+        scrNumRow.gravity = Gravity.CENTER
+        scrNumRow.textSize = 15F
+
+        scrStep.text = stepInput
+        scrStep.textSize = 15F
+
+        val stepParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT, 1f
+        )
+        preparationTableRow.addView(scrNumRow)
+//        scrStep.layoutParams(param)
+        preparationTableRow.addView(scrStep, stepParams)
+
+
+        preparationTableRow.setPadding(10, 10, 10, 10)
+        preparationTableRow.setBackgroundColor(resources.getColor(R.color.rowColor))
+
+        preparationTableLayout.addView(preparationTableRow)
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_SELECT_INGREDIENTS && resultCode == Activity.RESULT_OK && data != null) {
-            val quantityVal = data.getStringExtra(QUANTITY_KEY)
-            val unitVal = data.getStringExtra(UNIT_KEY)
+            val quantityVal = data.getStringExtra(UNIT_KEY)
+            val unitVal = data.getStringExtra(QUANTITY_KEY)
             val ingredientVal = data.getStringExtra(INGREDIENT_KEY)
             if (!quantityVal.isNullOrEmpty() && !unitVal.isNullOrEmpty() && !ingredientVal.isNullOrEmpty()) {
-                addNewRow(quantityVal, unitVal, ingredientVal)
-                counterRowTable += 1
+                addNewIngredientRow(quantityVal, unitVal, ingredientVal)
+                counterIngredientRowTable += 1
             }
         }
     }
