bbfe83c17da4a2f7d45a3314b89d747ed7e2a16a
enavhasson <enav.hasson@cs.huji.ac.il>
2020-10-01 23:42:49 +0300

    Update ScrollingActivity.kt
    its onlylook that I change alot , I only add rows on start and new fun

--- app/src/main/java/com/example/cocktails/ScrollingActivity.kt
+++ app/src/main/java/com/example/cocktails/ScrollingActivity.kt
@@ -13,6 +13,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import android.view.*
 import android.view.View.OnFocusChangeListener
 import android.view.inputmethod.InputMethodManager
@@ -29,6 +30,7 @@ import com.example.cocktails.CustomItem.UserItemLevel1
 import com.example.cocktails.R.*
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
+import com.google.firebase.auth.FirebaseUser
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.parcel.Parcelize
@@ -40,16 +42,22 @@ import ru.nikartm.support.ImageBadgeView
 
 @Parcelize
 data class Cocktail(
-    val name: String, val type: String, val steps:  ArrayList<String>,
+    val name: String, val type: String, val steps: ArrayList<String>,
     val ingredients: ArrayList<String>, val clipart: String, val image: String?,
-    val isCustom: Boolean = false, val glass: String?, val isReview:Boolean=false, val rotation:Float=0F):Parcelable{
-    constructor() : this("","",ArrayList<String>(),ArrayList<String>(),"",null,false,null,false,0F)}
+    val isCustom: Boolean = false, val glass: String?, val isReview: Boolean = false,
+    val rotation: Float = 0F
+) : Parcelable {
+    constructor() : this(
+        "", "", ArrayList<String>(), ArrayList<String>(), "",
+        null, false, null, false, 0F
+    )
+}
 
 class ScrollingActivity : AppCompatActivity() {
 
     private var gridViewAdapter: CocktailItemAdapter? = null
     private lateinit var recyclerView: RecyclerView
-    private var filterDialog : AlertDialog? = null
+    private var filterDialog: AlertDialog? = null
     private var filterIcon: View? = null
     private lateinit var menu: Menu
 
@@ -68,7 +76,14 @@ class ScrollingActivity : AppCompatActivity() {
 
     public override fun onStart() {
         super.onStart()
-        val mAuth = (applicationContext.applicationContext as Cocktails).mFAuth
+        val user: FirebaseUser? =
+            (applicationContext.applicationContext as Cocktails).mFAuth.currentUser
+        if (user != null) {
+            // do your stuff
+        } else {
+            signInAnonymously()
+        }
+//        val mAuth = (applicationContext.applicationContext as Cocktails).mFAuth
 //        mAuth.signInAnonymously()
 //            .addOnCompleteListener { task ->
 //                if (task.isSuccessful) {
@@ -80,6 +95,15 @@ class ScrollingActivity : AppCompatActivity() {
 //            }
     }
 
+    @SuppressLint("LongLogTag", "LogNotTimber")
+    private fun signInAnonymously() {
+        (applicationContext.applicationContext as Cocktails).mFAuth.signInAnonymously()
+            .addOnCompleteListener(this) { }.addOnFailureListener {
+                Log.e("signInAnonymously:FAILURE", "")
+            }
+
+    }
+
     private fun initRecyclerview() {
         recyclerView = findViewById(id.recyclerView)
         recyclerView.layoutManager = GridLayoutManager(this, getColumnNum())
@@ -87,7 +111,7 @@ class ScrollingActivity : AppCompatActivity() {
         recyclerView.adapter = gridViewAdapter
         recyclerView.itemAnimator = null
         recyclerView.isNestedScrollingEnabled = false
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() { })
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {})
     }
 
 
@@ -99,16 +123,18 @@ class ScrollingActivity : AppCompatActivity() {
         }
     }
 
-    private fun getColumnNum() : Int {
+    private fun getColumnNum(): Int {
         //        val columns = resources.getInteger(R.integer.gridColumnNum)
         val screenWidthDp: Float = resources.displayMetrics.widthPixels / resources.displayMetrics.density
-       return ((screenWidthDp / 202 + 0.5).toInt()) // +0.5 for correct rounding to int.
+        return ((screenWidthDp / 202 + 0.5).toInt()) // +0.5 for correct rounding to int.
     }
 
     private fun initAdapter() {
-        val jsonString = applicationContext.assets.open("predefined.json").bufferedReader().use { it.readText() }
+        val jsonString =
+            applicationContext.assets.open("predefined.json").bufferedReader().use { it.readText() }
         val listCocktailType = object : TypeToken<List<Cocktail>>() {}.type
-        val items :ArrayList<Cocktail> = Gson().fromJson<ArrayList<Cocktail>>(jsonString, listCocktailType)
+        val items: ArrayList<Cocktail> =
+            Gson().fromJson<ArrayList<Cocktail>>(jsonString, listCocktailType)
 
         gridViewAdapter = CocktailItemAdapter(this, items)
 
@@ -121,7 +147,8 @@ class ScrollingActivity : AppCompatActivity() {
         gridViewAdapter!!.registerAdapterDataObserver(object : AdapterDataObserver() {
             override fun onChanged() {
                 super.onChanged()
-                no_data_textView.visibility = if (gridViewAdapter!!.itemCount == 0) View.VISIBLE else View.INVISIBLE
+                no_data_textView.visibility =
+                    if (gridViewAdapter!!.itemCount == 0) View.VISIBLE else View.INVISIBLE
             }
         })
     }
@@ -142,8 +169,10 @@ class ScrollingActivity : AppCompatActivity() {
     private fun refreshFilterBadge() {
         if (filterIcon != null) {
             val cnt = (applicationContext as Cocktails)
-            val isFilterOff = cnt.mActiveTypeFilters.all.keys.isEmpty() && cnt.mActiveIngredientsFilters.all.keys.isEmpty()
-            filterIcon?.findViewById<ImageBadgeView>(id.filter_icon)?.badgeValue = if(isFilterOff) 0 else 1
+            val isFilterOff =
+                cnt.mActiveTypeFilters.all.keys.isEmpty() && cnt.mActiveIngredientsFilters.all.keys.isEmpty()
+            filterIcon?.findViewById<ImageBadgeView>(id.filter_icon)?.badgeValue =
+                if (isFilterOff) 0 else 1
         }
     }
 
@@ -152,28 +181,35 @@ class ScrollingActivity : AppCompatActivity() {
     private fun initSearchView(menu: Menu) {
         val searchView: SearchView = menu.findItem(id.action_search).actionView as SearchView
         searchView.findViewById<AutoCompleteTextView>(id.search_src_text).threshold = 1
-        val searchPlate = searchView.findViewById(androidx.appcompat.R.id.search_src_text) as EditText
+        val searchPlate =
+            searchView.findViewById(androidx.appcompat.R.id.search_src_text) as EditText
         searchPlate.hint = "Search"
         searchPlate.setTextAppearance(style.chipText)
         searchPlate.background = resources.getDrawable(drawable.rounded_corner, theme)
         searchPlate.setHintTextColor(Color.parseColor("#D5D3D3"))
         searchPlate.setTextColor(Color.parseColor("#D5D3D3"))
         val searchPlateView: View = searchView.findViewById(androidx.appcompat.R.id.search_plate)
-        searchPlateView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent))
+        searchPlateView.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                android.R.color.transparent
+            )
+        )
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
+
             override fun onQueryTextChange(newText: String?): Boolean {
                 gridViewAdapter?.filter?.filter(newText)
                 return false
             }
         })
-        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
         searchPlate.onFocusChangeListener = OnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
-                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(v.windowToken, 0)
             }
         }
@@ -187,7 +223,8 @@ class ScrollingActivity : AppCompatActivity() {
                 v.getGlobalVisibleRect(outRect)
                 if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
                     v.clearFocus()
-                    val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    val imm: InputMethodManager =
+                        getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
                 }
             }
@@ -206,7 +243,7 @@ class ScrollingActivity : AppCompatActivity() {
             id.action_myCocktails -> openFilteredActivityView("custom")
             id.action_help -> openContactUsActivity()
             android.R.id.home -> {
-                val options: ActivityOptions = ActivityOptions.makeCustomAnimation(this, 0,0)
+                val options: ActivityOptions = ActivityOptions.makeCustomAnimation(this, 0, 0)
                 finish()
                 this.startActivity(intent, options.toBundle())
                 return true
@@ -215,7 +252,7 @@ class ScrollingActivity : AppCompatActivity() {
         }
     }
 
-    private fun openFilteredActivityView(filter: String) : Boolean {
+    private fun openFilteredActivityView(filter: String): Boolean {
         val filteredActivity = Intent(this, ScrollingActivity::class.java)
         filteredActivity.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
         filteredActivity.putExtra("FilteredActivity", filter)
@@ -224,8 +261,10 @@ class ScrollingActivity : AppCompatActivity() {
         startActivity(filteredActivity)
 
         toolbar.menu.setGroupVisible(id.menuGroup, false)
-        toolbar_layout.title = if (filter == "favorites") resources.getString(string.favorites) else resources.getString(
-            string.my_cocktails)
+        toolbar_layout.title =
+            if (filter == "favorites") resources.getString(string.favorites) else resources.getString(
+                string.my_cocktails
+            )
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
@@ -240,8 +279,20 @@ class ScrollingActivity : AppCompatActivity() {
         val ingredientsFiltersSP = (applicationContext as Cocktails).mActiveIngredientsFilters
 
         val layout = layoutInflater.inflate(layout.filter_dialog, null) as ScrollView
-        resources.getStringArray(array.cocktailTypes_array).forEach { addChip(it, layout.CategoryChipGroup, typeFiltersSP) }
-        resources.getStringArray(array.ingredients).forEach { addChip(it, layout.IngredientChipGroup, ingredientsFiltersSP) }
+        resources.getStringArray(array.cocktailTypes_array).forEach {
+            addChip(
+                it,
+                layout.CategoryChipGroup,
+                typeFiltersSP
+            )
+        }
+        resources.getStringArray(array.ingredients).forEach {
+            addChip(
+                it,
+                layout.IngredientChipGroup,
+                ingredientsFiltersSP
+            )
+        }
 
         layout.findViewById<Button>(id.apply_btn).setOnClickListener {
             val typeFiltersSpEditor = typeFiltersSP.edit()
@@ -261,7 +312,7 @@ class ScrollingActivity : AppCompatActivity() {
             ingredientsFiltersSpEditor.apply()
             gridViewAdapter?.filter?.filter("filterDialog")
             refreshFilterBadge()
-            (menu.findItem(id.action_search).actionView as SearchView).setQuery("",true)
+            (menu.findItem(id.action_search).actionView as SearchView).setQuery("", true)
             filterDialog?.dismiss()
         }
 
@@ -304,11 +355,12 @@ class ScrollingActivity : AppCompatActivity() {
         chipGroup.addView(chip)
     }
 
-    private fun initUserItemButton(){
-        val userItemButton:View= findViewById(id.userItemFab)
+    private fun initUserItemButton() {
+        val userItemButton: View = findViewById(id.userItemFab)
         userItemButton.setOnClickListener {
-        val createNewItemIntent = Intent(applicationContext, UserItemLevel1::class.java)
-        startActivity(createNewItemIntent) }
+            val createNewItemIntent = Intent(applicationContext, UserItemLevel1::class.java)
+            startActivity(createNewItemIntent)
+        }
     }
 
     private fun openContactUsActivity(): Boolean {
